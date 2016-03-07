6ef84bf64c0e14589ca15cdb860af3d3d2383bb9
Magnus Mischel <magnus@misec.net>
2016-03-07 12:35:34 +0100

    Validate uniqueness of company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
   validates :name, presence: true
-  validates :number, presence: true
+  validates :number, presence: true, uniqueness: true
   validates_format_of :number, :with => /\d{10}/
 end
